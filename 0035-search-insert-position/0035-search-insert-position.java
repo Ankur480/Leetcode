defd1ff2b9e5ef44bbe143f3ca344a77423d2179
Ankur Mondal <ankurmondal039@gmail.com>
2026-05-06 14:52:06 +0530

    Time: 0 ms (100%), Space: 45.1 MB (12.16%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int storeindex = -1;
+        while(left <= right)
+        {
+            int mid = left + (right-left) / 2;
+            if(nums[mid]>=target) {
+                storeindex = mid;
+                right = mid-1;
+            }
+            else left = mid+1;
+        }
+        return storeindex != -1? storeindex : nums.length;
+    }
+}
